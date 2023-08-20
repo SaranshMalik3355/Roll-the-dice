d9d28fd471c037bbe99b68ff44414735aa49405d
Saransh Malik <saransh.malik.cs.2019@miet.ac.in>
2023-08-20 21:51:39 +0530

    Final commit

--- Readme.md
+++ Readme.md
@@ -16,4 +16,5 @@
 2. <p>We will be rotating the main dice. To make rotation to any element in css we use CSS property named as <b>transform:rotate(60deg)</b> </p>
 
 3. <p> We will be doing the same work with animation to rotate dice image from 0deg to 360 deg.</p>
-4. <p>We make animation through @keyframes. Keyframes take an identifier which is name of our animation.</p>
+4. <p>We make animation through @keyframes. Keyframes take an identifier which is the name of our animation.</p>
+5. <p>While making the animation we will see that the image of dice has been changed which we don't want, to tackle this situation.We will use setTimeout function to delay the execution of<b>image.src = `images/dices${rndDice}.png</b>. So that the dice animation takes the given time </p>
---
+++ app.js
@@ -0,0 +1,63 @@
+const btnEven = document.querySelector(".btn-even");
+const btnOdd = document.querySelector(".btn-odd");
+const btnThrow = document.querySelector(".dice-throw");
+btnThrow.disabled = true;
+const image = document.querySelector(".dice-image");
+const result = document.querySelector(".result");
+
+let userSelection = "";
+
+btnEven.addEventListener("click", () => {
+  userSelection = "even";
+  btnThrow.disabled = false;
+  btnEven.classList.add("btn-even-odd-select");
+  btnOdd.classList.remove("btn-even-odd-select");
+
+  //remove animation
+  image.classList.remove("dice-spin");
+
+  // Reset cover image
+  image.src = `images/dice-question.png`;
+});
+
+btnOdd.addEventListener("click", () => {
+  userSelection = "odd";
+  btnThrow.disabled = false;
+  btnOdd.classList.add("btn-even-odd-select");
+  btnEven.classList.remove("btn-even-odd-select");
+
+  //remove animation
+  image.classList.remove("dice-spin");
+
+  // Reset cover image
+  image.src = `images/dice-question.png`;
+});
+
+btnThrow.addEventListener("click", () => {
+  console.log(userSelection);
+
+  // Disabled itself
+  btnThrow.disabled = true;
+  //Random number
+  let rndDice = Math.ceil(Math.random() * 6);
+
+  //Adding animation
+  image.classList.add("dice-spin");
+
+  //SetTimeout
+  setTimeout(() => {
+    //apply dice image
+    image.src = `images/dice${rndDice}.png`;
+
+    // adding logic
+    if (rndDice % 2 == 0 && userSelection == "even") {
+      result.innerHTML = "You win you selected even";
+    } else if (rndDice % 2 != 0 && userSelection == "odd") {
+      result.innerHTML = "You win you selected odd";
+    } else if (rndDice % 2 == 0 && userSelection == "odd") {
+      result.innerHTML = "You lose!! you selected odd";
+    } else if (rndDice % 2 != 0 && userSelection == "even") {
+      result.innerHTML = "You lose!! you selected even";
+    }
+  }, 4000);
+});
